f41e778e80836938d78ef81e043d321c10161e0e
ntbloom <ntbloom@gmail.com>
2021-08-11 23:57:30 -0400

    protobuf is haunted

--- learnprotobuf.ino
+++ learnprotobuf.ino
@@ -1,37 +1,53 @@
 #include "src/pb_encode.h"
 #include "src/raincounter.pb.h"
 
-
 static pb_ostream_t stream;
 static uint8_t buffer[64];
 static DataPacket packet;
-static int32_t tempF = 72;
+static int32_t tempF = 120;
 static int32_t tempC = 25;
+bool success;
+
+static uint8_t error[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void setup() {
     Serial.begin(9600);
 }
 
 void sendData(bool metric) {
-    if (metric) {
-        packet.value = tempC--;
-        packet.packetType = DataPacket_PacketType_TEMPERATURE_C;
-    } else {
-        packet.value = tempF--;
-        packet.packetType = DataPacket_PacketType_TEMPERATURE_F;
-    }
+    // if (metric) {
+    //    packet.value = tempC--;
+    //   packet.packetType = DataPacket_PacketType_TEMPERATURE_C;
+    //    } else {
+
+    // if (tempF == 10) {
+    //   tempF--;
+    //  return;
+    //}
+
+    packet.value = tempF--;
+    // packet.value = 10;
+
+    packet.packetType = DataPacket_PacketType_TEMPERATURE_F;
+    //   }
 
     stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    pb_encode(&stream, DataPacket_fields, &packet);
-    Serial.write(buffer,stream.bytes_written);
-    Serial.print("\n");
+    success = pb_encode(&stream, DataPacket_fields, &packet);
+    if (!success) {
+        // Serial.println(stream.errmsg);
+        // Serial.println(tempF);
+        Serial.write(error, sizeof(error));
+    } else {
+        Serial.write(buffer, stream.bytes_written);
+        Serial.print("\n");
+    }
 }
 
 void loop() {
     if (tempF <= -40) tempF = 72;
     if (tempC <= -40) tempC = 25;
     sendData(true);
-    sendData(false);
-    delay(1000);
+    //  sendData(false);
+    // delay(100);
 }
 
--- python/counter.py
+++ python/counter.py
@@ -1,12 +1,34 @@
-from raincounter_pb2 import DataPacket
+from raincounter_pb2 import DataPacket, _message
 import serial
 
 
 if __name__ == "__main__":
+    good = 0
+    bad = 0
+    bads = set()
+    lastgoods = set()
+    lastgood = 0
+    lastbinary = None
+    lastbinaries = set()
     with serial.Serial("/dev/ttyACM98", 9600) as serial:
-        for i in range(25):
+        # for i in range(25):
+        for i in range(1001):
             message = DataPacket()
-            binary = serial.readline().rstrip()
-            print(f"{binary=}")
-            message.ParseFromString(binary)
-            print(f"{message.value=}, {message.packetType=}\n")
+            binary = serial.readline()[0:-1]
+            # print(f"{binary=}")
+            try:
+                message.ParseFromString(binary)
+                # print(f"{message.value=}, {message.packetType=}\n")
+                good += 1
+                lastgood = message.value
+                lastbinary = binary
+            except Exception as e:
+                # print(f"{binary=}")
+                bad += 1
+                bads.add(binary)
+                lastgoods.add(lastgood)
+                lastbinaries.add(lastbinary)
+        print(f"{bads=}")
+        print(f"{lastgoods=}")
+        print(f"{lastbinaries=}")
+        print(f"{bad=},{good=}")
